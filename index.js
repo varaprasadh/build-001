f5dcf77e1a783bd9cda114026487f23b244252ef
varaprasadh <varaprasadh.a@gmail.com>
2022-04-03 13:00:46 +0530

    update port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
     return res.sendFile(__dirname + '/index.html');
 });
-const port = process.env.PORT || 3000;
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`listening on port ${port}`);
 });
